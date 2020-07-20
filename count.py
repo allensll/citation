a4891d597a133851f14cc6a54221e9979cac3e0f
allenslllive <sunll90@live.com>
2020-07-20 20:35:18 +0800

    init

--- count.py
+++ count.py
@@ -1,47 +1,78 @@
 import os
 
 import numpy as np
-import pandas
+import pandas as pd
+
+path = 'data/cit-HepPh/cit-hepPh/raw'
 
 
+def count():
 
-
-def main():
-
-    path = 'data/cit-HepPh/cit-hepPh/raw'
     files = [os.path.join(path, 'cit-HepPh.txt'), os.path.join(path, 'cit-HepPh-dates.txt')]
     skiprows = [4, 1]
 
-    cit = pandas.read_csv(files[0], sep='\t', header=None, skiprows=skiprows[0], dtype=str).values
-    date = pandas.read_csv(files[1], sep='\t', header=None, skiprows=skiprows[1], dtype=str)
+    cit = pd.read_csv(files[0], sep='\t', header=None, skiprows=skiprows[0], dtype=str).values
+    date = pd.read_csv(files[1], sep='\t', header=None, skiprows=skiprows[1], dtype=str)
     arr_date = date.values
-
     date.columns = ['id', 'date']
     date.set_index(['id'], inplace=True)
 
-    res = np.zeros((arr_date.shape[0], 11))
+    ids = list()
     for i in range(arr_date.shape[0]):
-        res[i][0] = int(arr_date[i][0])
+        ids.append(arr_date[i][0])
 
-    # dict_date = date.to_dict()
-    res = pandas.DataFrame(res)
-    date.columns = ['id', '92', '93', '94', '95', '96', '97', '98', '99', '00', '01']
+    zeros = np.zeros((arr_date.shape[0], 10), dtype=np.int)
+    df = pd.DataFrame(zeros)
+    df.columns = ['1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001']
+    df['id'] = ids
+    df.set_index(['id'], inplace=True)
 
-    res = pandas.DataFrame(res, columns=['id', '92', '93', '94', '95', '96', '97', '98', '99', '00', '01'], index=['id'])
+    for i in range(cit.shape[0]):
 
-    # for i in range(cit.shape[0]):
-    #     source = cit[i][0]
-    #     cite = cit[i][1]
-    #     j = dict_date[source]
-    #     j = dict_date[source][:4]
-    #     if j is not '2002':
-    #         j = int(j) - 1991
-    #
-    #         res[cite][j] += 1
+        if i % 10000 == 0:
+            # break
+            print(i)
 
-    print(res.shape)
+        source = cit[i][0]
+        cite = cit[i][1]
+
+        # clean data
+        ls = len(source)
+        lc = len(cite)
+        if ls < 7:
+            source = '0' * (7-ls) + source
+        if lc < 7:
+            cite = '0' * (7-lc) + cite
+        if source not in df.index or cite not in df.index:
+            continue
+        # end clean
+
+        j = date.loc[source, 'date'][:4]
+        if not j == '2002':
+            df.loc[cite, j] = df.loc[cite, j] + 1
+
+    df.to_csv(os.path.join(path, 'cit-count2.csv'))
+
+
+def clean():
+
+    # df = pd.read_csv(os.path.join(path, 'cit-count.csv'), dtype=str, header=0, index_col=None)
+    # df.set_index(['id'], inplace=True)
+    # print(df.shape)
+
+    df = pd.read_csv(os.path.join(path, 'cit-count.csv'), dtype=str, header=0)
+
+    print(df.shape)
+    for index, row in df.iterrows():
+        if row['id'][:2] == '11' or row['id'][:2] == '02':
+            df.drop(index=[index], inplace=True)
+
+    df.to_csv(os.path.join(path, 'cit-clean.csv'), index=None)
 
 
 if __name__ == '__main__':
-    main()
+
+    # count()
+
+    clean()
 
--- lstm.py
+++ lstm.py
@@ -1,23 +1,107 @@
+import os
+import numpy as np
+import pandas as pd
+
 import torch
 import torch.nn as nn
+import torch.optim as optim
+
+path = 'data/cit-HepPh/cit-hepPh/raw'
+
+np.random.seed(147)
+torch.manual_seed(147)
 
 
-def process():
+def gen_dataset():
+
+    df = pd.read_csv(os.path.join(path, 'cit-clean.csv'), dtype=str, header=0)
+    df = df.sample(frac=1, random_state=147)
+    df = df.values
+
+    cit = df[:, 1:].astype('float64')
+    ids = df[:, 0]
+
+    for i in range(cit.shape[0]):
+        id = ids[i]
+        year = '19'+id[:2] if id[0] == '9' else '20'+id[:2]
+        pads = int(year) - 1992
+        for j in range(pads):
+            cit[i, j] = -1
+
+    train_input = torch.from_numpy(cit[:20000, :-1])
+    train_target = torch.from_numpy(cit[:20000, 1:])
+    test_input = torch.from_numpy(cit[20000:, :-1])
+    test_target = torch.from_numpy(cit[20000:, 1:])
+
+    return train_input, train_target, test_input, test_target, ids
 
 
+class SeqNet(nn.Module):
+    def __init__(self):
+        super(SeqNet, self).__init__()
+        self.lstm1 = nn.LSTMCell(1, 51)
+        self.lstm2 = nn.LSTMCell(51, 51)
+        self.linear =nn.Linear(51, 1)
 
-class net:
-    pass
+    def forward(self, x, future=0):
+        outputs = []
+        h_t = torch.zeros(x.size(0), 51, dtype=torch.double)
+        c_t = torch.zeros(x.size(0), 51, dtype=torch.double)
+        h_t2 = torch.zeros(x.size(0), 51, dtype=torch.double)
+        c_t2 = torch.zeros(x.size(0), 51, dtype=torch.double)
+
+        for i, input_t in enumerate(x.chunk(x.size(1), dim=1)):
+            h_t, c_t = self.lstm1(input_t, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            output = self.linear(h_t2)
+            outputs += [output]
+        for i in range(future):  # if we should predict the future
+            h_t, c_t = self.lstm1(output, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            output = self.linear(h_t2)
+            outputs += [output]
+        outputs = torch.stack(outputs, 1).squeeze(2)
+        return outputs
 
 
+def main():
 
-def train():
-    pass
+    epoch = 10
+    train_input, train_target, test_input, test_target, _ = gen_dataset()
 
+    net = SeqNet()
+    net.double()
+    # net.load_state_dict(torch.load('pretrained/lstm.pt'))
+    criterion = nn.MSELoss()
+    # use LBFGS as optimizer since we can load the whole data to train
+    optimizer = optim.LBFGS(net.parameters(), lr=1)
+    # optimizer = optim.LBFGS(net.parameters(), lr=1)
+    # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[0.5 * epoch, 0.75 * epoch],
+    #                                                  gamma=0.3)
 
-def test():
-    pass
+    for i in range(epoch):
 
+        print('Epoch: ', i)
+
+        def closure():
+            optimizer.zero_grad()
+            out = net(train_input)
+            loss = criterion(out, train_target)
+            print('train loss:', loss.item())
+            loss.backward()
+            return loss
+        optimizer.step(closure)
+
+        # begin to predict, no need to track gradient here
+        with torch.no_grad():
+            future = 10
+            pred = net(test_input, future=future)
+            loss = criterion(pred[:, :-future], test_target)
+            print('test loss:', loss.item())
+            y = pred.detach().numpy()
+
+        # scheduler.step()
+    torch.save(net.state_dict(), 'pretrained/lstm_lbfgs.pt')
 
 
 if __name__ == '__main__':
