72af8747597ba82ca8fc937ea3c6a5e38a52b7c2
allenslllive <sunll90@live.com>
2020-07-22 22:27:11 +0800

    init

--- lstm.py
+++ lstm.py
@@ -5,6 +5,7 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.nn.utils.rnn import pack_sequence
 
 path = 'data/cit-HepPh/cit-hepPh/raw'
 
@@ -28,10 +29,42 @@ def gen_dataset():
         for j in range(pads):
             cit[i, j] = -1
 
-    train_input = torch.from_numpy(cit[:20000, :-1])
-    train_target = torch.from_numpy(cit[:20000, 1:])
-    test_input = torch.from_numpy(cit[20000:, :-1])
-    test_target = torch.from_numpy(cit[20000:, 1:])
+    n_train = 20000
+
+    train_input = list()
+    train_target = list()
+    for i in range(n_train):
+        x = np.delete(cit[i], np.where(cit[i] == -1))
+        if len(x) == 1:
+            continue
+        train_input.append(torch.Tensor(x[:-1]))
+        train_target.append(torch.Tensor(x[1:]))
+
+    train_input.sort(key=lambda data: len(data), reverse=True)
+    train_target.sort(key=lambda data: len(data), reverse=True)
+
+    train_input = pack_sequence(train_input)
+    train_target = pack_sequence(train_input)
+
+    test_input = list()
+    test_target = list()
+    for i in range(n_train, cit.shape[0]):
+        x = np.delete(cit[i], np.where(cit[i] == -1))
+        if len(x) == 1:
+            continue
+        test_input.append(torch.Tensor(x[:-1]))
+        test_target.append(torch.Tensor(x[1:]))
+
+    test_input.sort(key=lambda data: len(data), reverse=True)
+    test_target.sort(key=lambda data: len(data), reverse=True)
+
+    test_input = pack_sequence(test_input)
+    test_target = pack_sequence(test_target)
+
+    # train_input = torch.from_numpy(cit[:n_train, :-1])
+    # train_target = torch.from_numpy(cit[:n_train, 1:])
+    # test_input = torch.from_numpy(cit[n_train:, :-1])
+    # test_target = torch.from_numpy(cit[n_train:, 1:])
 
     return train_input, train_target, test_input, test_target, ids
 
@@ -41,7 +74,7 @@ class SeqNet(nn.Module):
         super(SeqNet, self).__init__()
         self.lstm1 = nn.LSTMCell(1, 51)
         self.lstm2 = nn.LSTMCell(51, 51)
-        self.linear =nn.Linear(51, 1)
+        self.linear = nn.Linear(51, 1)
 
     def forward(self, x, future=0):
         outputs = []
@@ -64,6 +97,34 @@ class SeqNet(nn.Module):
         return outputs
 
 
+def train(net, optimizer, criterion, train_input, train_target):
+    def closure():
+        optimizer.zero_grad()
+        out = net(train_input)
+        loss = criterion(out, train_target)
+        print('train loss:', loss.item())
+        loss.backward()
+        return loss
+
+    optimizer.step(closure)
+
+
+def test(net, criterion, test_input, test_target):
+    net.eval()
+    with torch.no_grad():
+        future = 1
+        pred = net(test_input, future=future)
+        loss = criterion(pred[:, :-future], test_target)
+        print('test loss:', loss.item())
+        y = pred.detach().numpy()
+
+        # print(sum(sum(y[:, :-future]-test_target.numpy())**2)/9)
+        print(y)
+        print(np.around(y))
+
+        print(np.around(test_target.numpy()))
+
+
 def main():
 
     epoch = 10
@@ -76,33 +137,36 @@ def main():
     # use LBFGS as optimizer since we can load the whole data to train
     optimizer = optim.LBFGS(net.parameters(), lr=1)
     # optimizer = optim.LBFGS(net.parameters(), lr=1)
-    # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[0.5 * epoch, 0.75 * epoch],
-    #                                                  gamma=0.3)
+    # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[0.5 * epoch, 0.75 * epoch], gamma=0.3)
 
     for i in range(epoch):
 
         print('Epoch: ', i)
 
-        def closure():
-            optimizer.zero_grad()
-            out = net(train_input)
-            loss = criterion(out, train_target)
-            print('train loss:', loss.item())
-            loss.backward()
-            return loss
-        optimizer.step(closure)
+        train(net, optimizer, criterion, train_input, train_target)
 
-        # begin to predict, no need to track gradient here
-        with torch.no_grad():
-            future = 10
-            pred = net(test_input, future=future)
-            loss = criterion(pred[:, :-future], test_target)
-            print('test loss:', loss.item())
-            y = pred.detach().numpy()
+        test(net, criterion, test_input, test_target)
 
         # scheduler.step()
     torch.save(net.state_dict(), 'pretrained/lstm_lbfgs.pt')
 
 
+def eval():
+
+    train_input, train_target, test_input, test_target, _ = gen_dataset()
+
+    net = SeqNet()
+    net.double()
+    net.load_state_dict(torch.load('pretrained/lstm_lbfgs.pt'))
+    criterion = nn.MSELoss()
+
+    # test(net, criterion, test_input, test_target)
+
+    test(net, criterion, test_input[16:17], test_target[16:17])
+
+
 if __name__ == '__main__':
+
     main()
+
+    # eval()
--- tmp.py
+++ tmp.py
@@ -1,21 +1,10 @@
-import os
-import os.path as osp
-import pandas
-import numpy as np
 import torch
+from torch import nn
 
+import numpy as np
 
-def main():
+a = np.random.random([3, 1])
 
-    print(os.getcwd())
-    file = osp.join(os.curdir, 'data', 'cit-HepPh', 'cit-HepPh', 'raw', 'cit-HepPh.txt')
-    skiprows = 4
+print(a)
 
-    edge_index = pandas.read_csv(file, sep='\t', header=None, skiprows=skiprows, dtype=np.int64)
-
-    edge_index = torch.from_numpy(edge_index.values).t()
-    num_nodes = 34546
-
-
-if __name__ == '__main__':
-    main()
+print(np.around(a))
